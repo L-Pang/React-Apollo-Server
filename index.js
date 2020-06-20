ae9c9065bc76532dffa788271be0b7fad7e512e3
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-19 18:08:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,29 +20,14 @@ server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = createServer(app);
 server.installSubscriptionHandlers(httpServer);
 
-// httpServer.listen({ port: process.env.PORT || 4000, hostname: '0.0.0.0' }, () => {
-//     console.log('Apollo Server on http://localhost:' + process.env.PORT + '/ graphql');
-// });
-
-httpServer.listen({
-    port: 4000,
-}, () => {
-    console.log('Apollo Server on 4000/graphql');
+httpServer.listen({ port: process.env.PORT || 4000, hostname: '0.0.0.0' }, () => {
+    console.log('Apollo Server on http://localhost:' + process.env.PORT + '/ graphql');
 });
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-3pmsx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//     useNewUrlParser: true
-// });
-
-
-// client.connect(err => {
-//     // const collection = client.db("Foodie-db").collection("Foodie");
-//     db = client.db("Foodie-db")
-//     console.log("Connected correctly to server");
-//     // perform actions on the collection object
-//     client.close();
+// httpServer.listen({
+//     port: 4000,
+// }, () => {
+//     console.log('Apollo Server on 4000/graphql');
 // });
 
 const mongoose = require('mongoose');
