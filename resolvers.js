25bc869f2d680d6cacf1fa5c22438e7442d988e8
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-20 22:18:02 -0700

    Update server

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,4 @@
-const {
-    AuthenticationError
-} = require('apollo-server');
+const { AuthenticationError } = require('apollo-server');
 const faker = require('faker');
 const JsonWebToken = require('jsonwebtoken');
 const Bcrypt = require('bcryptjs');
@@ -9,6 +7,7 @@ const jwtSecret = '34%%##@#FGFKFL';
 
 const Product = require('./models/product');
 const Category = require('./models/category');
+const { ObjectId } = require('mongoose');
 // const User = require('./models/user');
 
 const isTokenValid = token => {
@@ -47,6 +46,28 @@ const mockProduct = (id = false) => ({
     category: mockCategory(),
 });
 
+// const getProduct = id => {
+//     console.log(mongoose.isValidObjectId(id))
+//     const product = Product.find({ name: "Caesar Salad" })
+//     // .then(doc => {
+//     //     console.log(doc)
+//     // })
+//     // .catch(err => {
+//     //     console.error(err)
+//     // })
+//     product.then(function (data) {
+//         data.forEach(function (data) {
+//             // console.log(data.id);
+//             // console.log(getRecord(data));
+//             data.toObject({ virtuals: true })
+//         });
+//     })
+// };
+
+async function getProduct(id) {
+    return await Product.findOne({ name: "Caesar Salad" }).exec();
+}
+
 let order = {
     total: 0,
     products: [],
@@ -68,12 +89,9 @@ const resolvers = {
                 console.error(err)
             }),
         products: () => Product.find({})
-                // .then(doc => {
-                //     console.log(doc)
-                // })
-                .catch(err => {
-                    console.error(err)
-                }),
+            .catch(err => {
+                console.error(err)
+            }),
         categories: () => Category.find({})
             .catch(err => {
                 console.error(err)
@@ -82,15 +100,26 @@ const resolvers = {
         order: () => order,
     },
     Mutation: {
+        // addToOrder: (_, {
+        //     id
+        // }) => {
+        //     order = {
+        //         ...order,
+        //         total: order.total + 1,
+        //         products: [...order.products, mockProduct(id)],
+        //     };
+        //     console.log(order)
+        //     return order;
+        // },
         addToOrder: (_, {
             id
         }) => {
             order = {
                 ...order,
                 total: order.total + 1,
-                products: [...order.products, mockProduct(id)],
+                products: [...order.products, getProduct(id)],
             };
-            console.log(order)
+            console.log(order, "line 131")
             return order;
         },
         completeOrder: (_, { }, {
