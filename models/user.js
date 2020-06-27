5aa6474f3807bb1e7fa8c172822fae495977203b
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-27 13:30:11 -0700

    Update server

--- models/user.js
+++ models/user.js
@@ -65,31 +65,31 @@ const userSchema = new Schema({
     collection: 'User'
 });
 
-// userSchema.pre('save', function() {
-//     const hashedPassword = bcrypt.hashSync(this.password, 12);
-//     this.password = hashedPassword;
-// });
-
-userSchema.pre('save', async function(next) {
-    //'this' refers to the current document about to be saved
-    const user = this;
-    //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
-    //your application becomes.
-    const hash = await bcrypt.hash(this.password, 10);
-    //Replace the plain text password with the hash and then store it
-    this.password = hash;
-    //Indicates we're done and moves on to the next middleware
-    next();
+userSchema.pre('save', function() {
+    const hashedPassword = bcrypt.hashSync(this.password, 12);
+    this.password = hashedPassword;
 });
 
-//We'll use this later on to make sure that the user trying to log in has the correct credentials
-userSchema.methods.isValidPassword = async function(password) {
-    const user = this;
-    //Hashes the password sent by the user for login and checks if the hashed password stored in the
-    //database matches the one sent. Returns true if it does else false.
-    const compare = await bcrypt.compare(password, user.password);
-    return compare;
-}
+// userSchema.pre('save', async function(next) {
+//     //'this' refers to the current document about to be saved
+//     const user = this;
+//     //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
+//     //your application becomes.
+//     const hash = await bcrypt.hash(this.password, 10);
+//     //Replace the plain text password with the hash and then store it
+//     this.password = hash;
+//     //Indicates we're done and moves on to the next middleware
+//     next();
+// });
+
+// //We'll use this later on to make sure that the user trying to log in has the correct credentials
+// userSchema.methods.isValidPassword = async function(password) {
+//     const user = this;
+//     //Hashes the password sent by the user for login and checks if the hashed password stored in the
+//     //database matches the one sent. Returns true if it does else false.
+//     const compare = await bcrypt.compare(password, user.password);
+//     return compare;
+// }
 
 
 // userSchema.plugin(passportLocalMongoose, {
