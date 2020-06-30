493f7feba2a5f44f74f105308cf6c3f6ba5dd18c
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-29 17:55:03 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -40,31 +40,6 @@ userSchema.pre('save', function() {
     this.password = hashedPassword;
 });
 
-// userSchema.pre('save', async function(next) {
-//     //'this' refers to the current document about to be saved
-//     const user = this;
-//     //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
-//     //your application becomes.
-//     const hash = await Bcrypt.hash(this.password, 10);
-//     //Replace the plain text password with the hash and then store it
-//     this.password = hash;
-//     //Indicates we're done and moves on to the next middleware
-//     next();
-// });
-
-// //We'll use this later on to make sure that the user trying to log in has the correct credentials
-// userSchema.methods.isValidPassword = async function(password) {
-//     const user = this;
-//     //Hashes the password sent by the user for login and checks if the hashed password stored in the
-//     //database matches the one sent. Returns true if it does else false.
-//     const compare = await Bcrypt.compare(password, user.password);
-//     return compare;
-// }
-
-
-// userSchema.plugin(passportLocalMongoose, {
-//     usernameField: 'email'
-// });
 
 // create and export the model
 module.exports = mongoose.model('User', userSchema)
